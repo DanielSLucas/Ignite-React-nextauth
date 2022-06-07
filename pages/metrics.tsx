ce07f64a5b257a911e2c95ca158186dc18c28c5e
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-07 15:13:46 -0300

    validando permissões (server)

---
+++ pages/metrics.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+
+import { setupAPIClient } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
+
+const Metrics: NextPage = () => {
+  return (
+    <>      
+      <h1>Métricas</h1>
+    </>
+  )
+}
+
+export default Metrics;
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me')    
+  
+  
+  
+  return {
+    props: {}
+  }
+}, {
+  permissions: ['metrics.list'],
+  roles: ['administrator'],
+})
--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,14 +1,22 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSTAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+}
 
 // high order function
 // usar junto com o GetServerSideProps nas páginas que podem ser acessadas por visitantes 
-export function withSSRAuth<T>(fn: GetServerSideProps<T>) {
+export function withSSRAuth<T>(fn: GetServerSideProps<T>, options?: WithSSTAuthOptions ) {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<T>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token'];
 
-    if (!cookies['nextauth.token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
@@ -17,6 +25,26 @@ export function withSSRAuth<T>(fn: GetServerSideProps<T>) {
       }
     }
     
+    if (options) {
+      const user = decode<{ permissions: string[], roles: string[] }>(token);
+      const { permissions, roles } = options;
+  
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          }
+        }
+      }
+    }
+
     try {
       return await fn(ctx);
     } catch (err) {
