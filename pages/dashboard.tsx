dd2d5344fccc6abcdd348d1faffe39a8b0226050
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-07 11:32:03 -0300

    recuperando estado de autenticação

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { api } from "../services/api";
 
 const DashBoard: NextPage = () => {
   const { user } = useContext(AuthContext);
   
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response.data))
+  },[])
+
   return (
     <h1>Dashboard: {user?.email}</h1>
   )
